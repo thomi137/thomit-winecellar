ce1c652b797d49e097daae0d541bc289aa7c94c4
Thomas Prosser <thomas@prosser.ch>
2015-12-10 18:17:24 +0100

    Resource Server and Web Security

--- src/main/java/com/thomit/winecellar/security/WineCellarResourceServerConfiguration.java
+++ src/main/java/com/thomit/winecellar/security/WineCellarResourceServerConfiguration.java
@@ -1,21 +1,27 @@
 package com.thomit.winecellar.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
+@Order(2)
 public class WineCellarResourceServerConfiguration extends
 		ResourceServerConfigurerAdapter {
 
 	@Override
-	public void configure(HttpSecurity http) throws Exception{
-		http.antMatcher("/**")
-		.authorizeRequests()
-		.anyRequest()
-		.authenticated();
+	public void configure(HttpSecurity http) throws Exception {
+		http.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
+				.requestMatchers()
+				.antMatchers("/oauth/users/**", "/oauth/clients/**").and()
+				.authorizeRequests().anyRequest().authenticated().and()
+				.antMatcher("/api/**").authorizeRequests().anyRequest()
+				.authenticated();
 	}
-	
+
 }
--- src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
+++ src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
@@ -1,22 +1,35 @@
 package com.thomit.winecellar.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@Order(3)
+@EnableWebSecurity
+@Order(6)
 public class WineCellarSecurityConfiguration extends
 		WebSecurityConfigurerAdapter {
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
-		.antMatchers("/**")
-		.permitAll();
-		
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("wine").password("secret")
+		.roles("USER");
 	}
 	
-
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+		.antMatchers("/api/**", "/oauth/**")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.csrf().disable();
+	}
+	
 }
