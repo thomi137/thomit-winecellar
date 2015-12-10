d18ec7ed5d7cdd8bf1e4507526c66f34e8d4ae71
Thomas Prosser <thomas@prosser.ch>
2015-12-10 17:02:58 +0100

    set up OAuth2

--- src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
+++ src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.thomit.winecellar.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@Order(3)
 public class WineCellarSecurityConfiguration extends
 		WebSecurityConfigurerAdapter {
 
