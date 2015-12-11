15e45fead69daa30b9377ced8a5030837cd4c1cf
Thomas Prosser <thomas@prosser.ch>
2015-12-11 10:52:16 +0100

    Class for Client persistence

---
+++ src/main/java/com/thomit/winecellar/models/Client.java
@@ -0,0 +1,99 @@
+package com.thomit.winecellar.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	
+	String tenantId;
+	String clientId;
+	String clientSecret;
+	String resourceIds;
+	String scopes;
+	String grantTypes;
+	String authorities;
+
+	public Client(String clientId, String clientSecret, String resourceIds,
+			String scopes, String grantTypes, String authorities) {
+		this.clientId = clientId;
+		this.clientSecret = clientSecret;
+		this.resourceIds = resourceIds;
+		this.scopes = scopes;
+		this.grantTypes = grantTypes;
+		this.authorities = authorities;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTenantId() {
+		return tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getClientSecret() {
+		return clientSecret;
+	}
+
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
+
+	public String getResourceIds() {
+		return resourceIds;
+	}
+
+	public void setResourceIds(String resourceIds) {
+		this.resourceIds = resourceIds;
+	}
+
+	public String getScopes() {
+		return scopes;
+	}
+
+	public void setScopes(String scopes) {
+		this.scopes = scopes;
+	}
+
+	public String getGrantTypes() {
+		return grantTypes;
+	}
+
+	public void setGrantTypes(String grantTypes) {
+		this.grantTypes = grantTypes;
+	}
+
+	public String getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(String authorities) {
+		this.authorities = authorities;
+	}
+
+}
---
+++ src/test/java/com/thomit/wincellar/security/test/WineCellarSecurityTest.java
@@ -0,0 +1,13 @@
+package com.thomit.wincellar.security.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.thomit.winecellar.WineCellarApp;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WineCellarApp.class)
+public class WineCellarSecurityTest {
+
+}
