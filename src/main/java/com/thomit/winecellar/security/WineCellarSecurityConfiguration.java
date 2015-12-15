6982ba3f7f57ed95df06a186c0663101434d6737
Thomas Prosser <thomas@prosser.ch>
2015-12-15 17:41:27 +0100

    Fixed wrong api path

--- src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
+++ src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
@@ -2,19 +2,21 @@ package com.thomit.winecellar.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.test.context.ActiveProfiles;
 
 @Configuration
 public class WineCellarSecurityConfiguration extends
 		WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception{
 		auth.inMemoryAuthentication().withUser("wine").password("secret").roles("USER");
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		//http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
