b1a4628a0ad3b18adf6f85b600c7308d1256a5a1
Thomas Prosser <thomas@prosser.ch>
2015-12-15 17:52:27 +0100

    more fixes

--- src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
+++ src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
@@ -2,12 +2,10 @@ package com.thomit.winecellar.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.test.context.ActiveProfiles;
 
 @Configuration
 @EnableGlobalAuthentication
