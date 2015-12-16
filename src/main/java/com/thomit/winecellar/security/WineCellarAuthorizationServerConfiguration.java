3c376872f82ff7edf7ca104a5495b1e2a9ffa09a
Thomas Prosser <thomas@prosser.ch>
2015-12-16 11:27:37 +0100

    Password Encoder, DB Table for Clients

--- src/main/java/com/thomit/winecellar/security/WineCellarAuthorizationServerConfiguration.java
+++ src/main/java/com/thomit/winecellar/security/WineCellarAuthorizationServerConfiguration.java
@@ -1,31 +1,42 @@
 package com.thomit.winecellar.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+
+import com.thomit.winecellar.repositories.AccountRepository;
+import com.thomit.winecellar.exceptions.ClientNotFoundException;
 
 @Configuration
 @EnableAuthorizationServer
 public class WineCellarAuthorizationServerConfiguration extends
 		AuthorizationServerConfigurerAdapter {
-
-	/*
-	@Override
-	public void configure(security)
-			throws Exception {
-		security.passwordEncoder(passwordEncoder);
-	}
-	*/
+	
+	@Autowired
+	private AccountRepository accountRepository;
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security)
+			throws Exception {
+		security.passwordEncoder(passwordEncoder);
+	}
+
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints)
@@ -34,29 +45,23 @@ public class WineCellarAuthorizationServerConfiguration extends
 		
 	}
 
-
-
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients)
 			throws Exception {
-		
-		clients.inMemory()
-			.withClient("wine")
-			.secret("secret")
-			.authorizedGrantTypes("client_credentials", "refresh_token", "password")
-			.scopes("read", "write");
-		
-		// clients.withClientDetails(getClientDetailsService());
+		clients.withClientDetails(getClientDetailsService());
 	}
-
-	/*
-	
-	
+		
 	@Bean
 	public ClientDetailsService getClientDetailsService() {
-		return new VZClientDetailsService();
-	}
+		return (clientId) -> accountRepository.findByAccountId(clientId)
+				.map(a ->{
+					BaseClientDetails baseClientDetails = new BaseClientDetails(a.getAccountId(), a.getResourceIds(), a.getScopes(), a.getGrantTypes(), a.getAuthorities(), a.getRedirectUris());
+					baseClientDetails.setClientSecret(a.getAccountSecret());
+					return baseClientDetails;
+				})
+				.orElseThrow(
+						() -> new ClientNotFoundException(clientId)
+				);
+		}
 	
-	*/
-
 }
--- src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
+++ src/main/java/com/thomit/winecellar/security/WineCellarSecurityConfiguration.java
@@ -1,14 +1,12 @@
 package com.thomit.winecellar.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableGlobalAuthentication
+@EnableWebSecurity
 public class WineCellarSecurityConfiguration extends
 		WebSecurityConfigurerAdapter {
 
