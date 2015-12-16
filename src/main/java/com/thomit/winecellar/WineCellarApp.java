0caeb7d23a665c3c07068e0b464b4f0230bdb3e2
Thomas Prosser <thomas@prosser.ch>
2015-12-16 11:27:45 +0100

    Cleanup

--- src/main/java/com/thomit/winecellar/WineCellarApp.java
+++ src/main/java/com/thomit/winecellar/WineCellarApp.java
@@ -2,7 +2,11 @@ package com.thomit.winecellar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,5 +14,11 @@ public class WineCellarApp {
 
     public static void main(String[] args) {
         SpringApplication.run(WineCellarApp.class, args);
+    }
+    
+    @Bean
+    PasswordEncoder passwordEncoder(){
+    	return new BCryptPasswordEncoder();
+    	
     }
 }
