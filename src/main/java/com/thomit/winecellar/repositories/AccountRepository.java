10baa3de86879bbd188478105cb6fa26a5c584c4
Thomas Prosser <thomas@prosser.ch>
2015-12-17 07:32:34 +0100

    Make sure the Repository is not exported

--- src/main/java/com/thomit/winecellar/repositories/AccountRepository.java
+++ src/main/java/com/thomit/winecellar/repositories/AccountRepository.java
@@ -3,9 +3,11 @@ package com.thomit.winecellar.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.thomit.winecellar.models.Account;
 
+@RestResource(exported=false)
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
 	Optional<Account> findByAccountId(String accountId);
